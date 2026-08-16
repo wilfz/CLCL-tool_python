4ea59d0daf32a77b4371358e00806975eb205e1b
wilfzim <48259856+wilfz@users.noreply.github.com>
2026-08-16 22:24:24 +0200

    added python_scripts/example.py

---
+++ python_scripts/example.py
@@ -0,0 +1,10 @@
+# By default, main is executed. input_text is the content of the clipboard.
+def main(input_text: str = ""):
+    ret = "Hello Python!"
+    return ret # return to CLCL
+
+def ToLower(s: str = ""):
+    return s.lower() # return a str to CLCL
+
+def ToUpper(s: str = ""):
+    return s.upper() # return a str to CLCL
